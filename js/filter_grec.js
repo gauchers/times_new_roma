8ce7e3a7b47c2602106f81c1d6c105ea3629b13a
Sarah Gaucher <sarah.gaucher@univ-grenoble-alpes.fr>
2026-04-28 09:15:10 +0200

    Refactor themes object in filter_grec.js

--- js/filter_grec.js
+++ js/filter_grec.js
@@ -39,8 +39,7 @@ const ANA_LABELS = {
         epyllion:              "Épyllion"
     },  
     
-    themes: {
-        agriculture: "Agriculture et vie rustique",
+        themes: {
         amitie:      "Amitié",
         amour:       "Amour et érotisme",
         argent:      "Argent et commerce",
@@ -48,17 +47,19 @@ const ANA_LABELS = {
         eloge:       "Éloge et blâme",
         famille:     "Famille et ancêtres",
         guerre:      "Guerre et paix",
-        litterature: "Littérature et arts",
+        alterite:    "Identité et altérité",
+        litterature: "Littérature, rhétorique et arts",
         mort:        "Mort",
-        nature:      "Nature et animaux",
+        nature:      "Nature",
         philosophie: "Philosophie et sagesse",
         politique:   "Politique et pouvoir",
-        religion:    "Religions et mythologie",
+        religions:   "Religions et mythologie",
         societe:     "Société",
-        voyage:      "Voyage et exil",
+        quotidien:    "Vie quotidienne et realia",
         violence:      "Violence et luttes"
     }
 };
+
 
 document.addEventListener("DOMContentLoaded", async () => {
 
